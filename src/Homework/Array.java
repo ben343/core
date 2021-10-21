872a5c280db8f211a370003d61ec9712ef2b9ae9
Admin <benkhachatryan77@mail.ru>
2021-10-21 23:48:19 +0400

    Վեջին տնային աշխատանքը

---
+++ src/Homework/Array.java
@@ -0,0 +1,35 @@
+package Homework;
+
+public class Array {
+    public static void main(String[] args) {
+        char[] text = {' ', ' ', 'b', 'a', 'r', ' ', 'e', 'v', ' ', ' '};
+        char[] resul = new char[text.length];
+        int index = 0;
+        int i;
+        for (i = 0; i < text.length; i++) {
+            if (text[i] == ' ') {
+                continue;
+            } else if (text[i] != ' ') {
+                index++;
+                resul[index] = text[i];
+                break;
+            }
+        }
+        for (int j = i; j < text.length; j++) {
+            if (text[j] == ' ') {
+
+                index++;
+                resul[index] = text[j];
+            } else if (text[j] != ' ') {
+                index++;
+                resul[index] = text[j];
+            }
+            System.out.print(resul[index]);
+        }
+
+    }
+
+}
+
+
+
