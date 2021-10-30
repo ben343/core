ae1dabc4a78fb934c392c7b27006700d4f43510f
Admin <benkhachatryan77@mail.ru>
2021-10-30 19:29:03 +0400

    Գրքի օրինակները

--- src/Lesson9/Box.java
+++ src/Lesson9/Box.java
@@ -10,7 +10,7 @@ public class Box {
         System.out.println(width * height * depth);
     }
 
-    double volume() {
+    double volume1() {
         return width * height * depth;
     }
 
@@ -18,6 +18,28 @@ public class Box {
         width = w;
         height = h;
         depth = d;
+    }
+
+    Box() {
+        System.out.println("Koнcтpyиpoвaниe объекта Вох");
+        width = 10;
+        height = 10;
+        depth = 10;
+
+    }
+
+    double volume() {
+        return width * height * depth;
+    }
+
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    double volume2() {
+        return width * height * depth;
     }
 }
 
---
+++ src/Lesson9/BoxDemo7.java
@@ -0,0 +1,14 @@
+package Lesson9;
+
+public class BoxDemo7 {
+    public static void main(String[] args) {
+        Box mybox1 = new Box(10, 20, 15);
+        Box mybox2 = new Box(3, 6, 9);
+
+        double vol;
+        vol = mybox1.volume2();
+        System.out.println(vol);
+        vol = mybox2.volume2();
+        System.out.println(vol);
+    }
+}
