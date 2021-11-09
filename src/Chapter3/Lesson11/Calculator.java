78c1d8e6caea78e6222535274543bb6c82241952
Admin <benkhachatryan77@mail.ru>
2021-11-10 00:59:56 +0400

    օրինակ

---
+++ src/Chapter3/Lesson11/Calculator.java
@@ -0,0 +1,16 @@
+package Chapter3.Lesson11;
+
+public class Calculator {
+public int gumarum (int a,int b){
+    return  a+b;
+}
+public int  hanum (int  a,int b) {
+      return (a -b);
+   }
+    public  int bazmapatkum (int a,int b) {
+    return a *b;
+    }
+    public  int bajanum (int a,int b){
+        return  a/b;
+    }
+ }
